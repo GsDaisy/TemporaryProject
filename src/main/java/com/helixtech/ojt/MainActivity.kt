806a4ca58d6357db48610d7745eb5ac9d3f9ded1
GsDaisy <dbstndud0422@naver.com>
2019-12-09 14:04:12 +0900

    [Java]My Firebase Messaging Service

--- src/main/java/com/helixtech/ojt/MainActivity.kt
+++ src/main/java/com/helixtech/ojt/MainActivity.kt
@@ -4,9 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.messaging.FirebaseMessaging
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +28,29 @@ class MainActivity : AppCompatActivity() {
             startActivity(nfcIntent);
         }
 
+        FirebaseMessaging.getInstance().isAutoInitEnabled = true
+
+        FirebaseInstanceId.getInstance().instanceId
+                .addOnCompleteListener(OnCompleteListener { task ->
+                    if (!task.isSuccessful) {
+                        Log.w(TAG, "getInstanceId failed", task.exception)
+                        return@OnCompleteListener
+                    }
+
+                    // Get new Instance ID token
+                    val token = task.result?.token
+
+                    // Log and toast
+                    //val msg = getString(R.string.msg_token_fmt, token)
+                    Log.d(TAG, token)
+                    Toast.makeText(baseContext, token, Toast.LENGTH_SHORT).show()
+                })
 
     }
+
+    companion object {
+        private const val TAG = "MainActivity"
+
+    }
+
 }
