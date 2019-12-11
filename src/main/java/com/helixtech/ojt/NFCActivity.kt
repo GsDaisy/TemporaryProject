acb1e0d7010ac11345c25b3385fc533168c49bf5
GsDaisy <dbstndud0422@naver.com>
2019-12-11 10:52:33 +0900

    [Kotlin]Read Message from NFC Tag

--- src/main/java/com/helixtech/ojt/NFCActivity.kt
+++ src/main/java/com/helixtech/ojt/NFCActivity.kt
@@ -2,22 +2,25 @@ package com.helixtech.ojt
 
 import android.app.PendingIntent
 import android.content.Intent
+import android.nfc.NdefMessage
 import android.nfc.NfcAdapter
 import android.nfc.Tag
-import android.os.Build
+import android.nfc.tech.Ndef
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
-import androidx.annotation.RequiresApi
 
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_nfc.*
 import java.lang.StringBuilder
+import java.nio.charset.Charset
 import kotlin.experimental.and
 
 class NFCActivity : AppCompatActivity() {
 
     internal var nfcAdapter: NfcAdapter? = null
     internal lateinit var pendingIntent: PendingIntent
+    internal var result: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,27 +50,67 @@ class NFCActivity : AppCompatActivity() {
 
     }
 
-    @RequiresApi(Build.VERSION_CODES.KITKAT)
     override fun onNewIntent(intent: Intent) {
         super.onNewIntent(intent)
 
         val tag = intent.getParcelableExtra<Tag>(NfcAdapter.EXTRA_TAG)
-        //val tag2 = intent.getParcelableExtra<Tag>(NfcAdapter.EXTRA_DATA)
+        val ndef = Ndef.get(tag)
+
+        val size = ndef.maxSize
+        val type = ndef.type
+
+        //val id = byteArrayToString(tag!!.id)
+
+        val byteData: ByteArray = tag.id
+
+        var sb: StringBuilder = StringBuilder();
+        for (i in byteData) {
+            sb.append(String.format("%02X", i and 0xff.toByte()))
+        }
+        val id:String = sb.toString()
+
+        tag_id_txt.text = id
+
+        try {
+            val messages = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)
+
+            Toast.makeText(this, "Here : $messages", Toast.LENGTH_SHORT).show()
+            val size = messages.size
+            Log.d("SIZEE", size.toString())
+            for (i in 0..1)
+                readTagData(messages[0] as NdefMessage)
+        } catch (e: Exception) {
+            Log.d("EEEROR", e.printStackTrace().toString())
+        }
+
+        Toast.makeText(this, "Data : $result", Toast.LENGTH_SHORT).show()
+        tag_data_txt.text = result
+
+    }
 
 
-        tag?.let {
+    fun readTagData(ndefmsg: NdefMessage) {
 
-            val byteData: ByteArray = tag.id
+        ndefmsg?.let {
 
-            var sb: StringBuilder = StringBuilder();
-            for (i in byteData) {
-                sb.append(String.format("%02X", i and 0xff.toByte()))
+            var msgs = ""
+            msgs += ndefmsg.toString() + "\n"
+            val records = ndefmsg.records
+
+            for (rec in records) {
+                val payload = rec.payload
+                var textEncoding = "UTF-8"
+                if (payload.size > 0)
+                    textEncoding = if ((payload[0] and 128.toByte()).toInt()==0) "UTF-8" else "UTF-16"
+
+                val tnf = rec.tnf
+                val type = rec.type.toString()
+                val payloadStr = String(rec.payload, Charset.forName(textEncoding))
+                result = payloadStr
+                Toast.makeText(this, "$tnf : $type : $payloadStr", Toast.LENGTH_SHORT).show()
+                /*Toast.makeText(this, "Data : $result", Toast.LENGTH_SHORT).show()
+                data_txt2.text = result*/
             }
-
-
-            Toast.makeText(this, sb.toString(), Toast.LENGTH_SHORT).show()
-            data_txt.text = sb.toString()
-
         }
     }
 
@@ -75,6 +118,6 @@ class NFCActivity : AppCompatActivity() {
 
      }*/
 
-    fun firstN(n: Int): Array<Int> = Array(n, {i->i*i})
+
 
 }
